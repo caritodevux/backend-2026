3c4ac1fe1b44571c9e93b1cfadfac6618ea879ae
unknown <sistemas@VRE_LAB20504.inacap.cl>
2026-08-24 20:13:23 -0400

    se configuro django admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Persona, Usuario, Rol, UsuarioRol
 
-# Register your models here.
+# Registramos los modelos básicos
+admin.site.register(Persona)
+admin.site.register(Usuario)
+admin.site.register(Rol)
+admin.site.register(UsuarioRol)
+
+#admin admin
